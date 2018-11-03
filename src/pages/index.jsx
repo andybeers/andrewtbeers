ef7b2796c74ca4a2b7d953e65c4c67925635d36a
Andrew Beers <andybeers@gmail.com>
2018-11-03 13:11:26 -0700

    Add image

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import helloPhoto from '../images/hellothere.png'
 
 import Layout from '../components/layout'
+import './index.css'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Andrew Beers</h1>
+    <p>JavaScript developer, fruity coffee lover, mushroom hater.</p>
+    <img src={helloPhoto} alt="Andrew Smiling" />
+    <Link to="/page-2/">mystery link</Link>
   </Layout>
 )
 
