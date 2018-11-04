b223e3e7f14c77700b03329a37b051a51045e8fb
Andrew Beers <andybeers@gmail.com>
2018-11-04 08:47:28 -0800

    adding colored box header

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,11 +5,19 @@ import helloPhoto from '../images/hellothere.png'
 import Layout from '../components/layout'
 import './index.css'
 
+const blocks = Array(10).fill()
+
 const IndexPage = () => (
   <Layout>
+    <div className="block-container">
+      {blocks.map((block, idx) => (
+        /* eslint-disable-next-line */
+        <div key={idx} className={`block color-${idx}`} />
+      ))}
+    </div>
     <h1>Andrew Beers</h1>
     <p>JavaScript developer, fruity coffee lover, mushroom hater.</p>
-    <img src={helloPhoto} alt="Andrew Smiling" />
+    {/* <img src={helloPhoto} alt="Andrew Smiling" /> */}
     <Link to="/page-2/">mystery link</Link>
   </Layout>
 )
